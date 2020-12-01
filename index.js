7970ca4568e8df9a44e023b97fd167f9827c26fe
KaleevaAlina <69644331+KaleevaAlina@users.noreply.github.com>
2020-12-02 00:40:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict"
+//Задача 2
+function butmouseover() {
+	var input = document.getElementById('input');
+	input.value = '!!!!!';
+}
+function butmouseout() {
+	var input = document.getElementById('input');
+	input.value = '???';
+} 
+//Задача 3
+function butDblclick() {
+	var input = document.getElementById('but');
+	alert('Привет!!!!!!!!!')
+}
+//Задача 4
+function but1mouseover() {
+}	
+function but1mouseout() {
+	alert('Привет!!!!!!!!!!!!');
+}	
+//Задача 5
+function button1Click() {
+	 alert('Задача 5'); 
+	/*  var button1 = document.getElementById('button1');
+	button1.value = 'Ой я поменял свой текст';  */
+}
